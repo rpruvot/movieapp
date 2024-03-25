c11212ec147937c1d4992696dd959ee8ac571ac1
MaxenPAULIN <163163168+MaxenPAULIN@users.noreply.github.com>
2024-03-25 08:27:45 +0100

    Add front end

---
+++ movieapp/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ConnectComponent } from './connect/connect.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
+
+const routes: Routes = [
+  {path:'connect', component: ConnectComponent},
+  {path:'createaccount', component: CreateaccountComponent},
+  {path:'', component: AcceuilComponent}  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ movieapp/src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ConnectComponent } from './connect/connect.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AcceuilComponent,
+    ConnectComponent,
+    CreateaccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"projetweb-92fe6","appId":"1:159905600169:web:20999195191238cdf0cfb8","storageBucket":"projetweb-92fe6.appspot.com","apiKey":"AIzaSyCG3KnX4M1bAaReghJLzlvagm57dSW6Gas","authDomain":"projetweb-92fe6.firebaseapp.com","messagingSenderId":"159905600169"})),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideDatabase(() => getDatabase()),
+    MatToolbarModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule    
+  ],
+  providers: [
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
